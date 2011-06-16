ce73d7e0453e7ef8017cc0b1fea96238612498a9
peter cutting <peter.cutting@gmail.com>
2011-06-16 21:49:29 +0200

    a real message

--- app/controllers/gearth_controller.rb
+++ app/controllers/gearth_controller.rb
@@ -32,20 +32,9 @@ class GearthController < ApplicationController
     return f
   end
 
+  #http://localhost:3000/gearth/igc
 
-  #http://localhost:3000/gearth/network_link3?path=public/data
-
-  def network_link3
-    #puts 'network_link3' + Constants::XX
-    path = "public/data"
-    path = "c:/Users/peter/workspace_rails/igcsmall"
-
-    path = params[:path] unless params[:path].nil?
-
-    #puts 'path ' + path
-    #puts 'p ' + Dir.pwd
-    puts 'network_link3 ' + params.inspect
-    @entries = []
+  def igc
     roots = []
 
     @igcfs = Igcfile.find(:all, :order => 'path')  # get files
@@ -54,17 +43,8 @@ class GearthController < ApplicationController
       update_substr(roots ,igcf.path)
     end
 
-    #puts roots.inspect
-
-    #igcps = igcf.igcpoint(:all, :order => 'seq_secs')   # get points
-    #igcps.each do |igcp|          # for each point
-    #@ps<<(igcp.rlon/RAD_PER_DEG).to_s + ',' + (igcp.rlat/RAD_PER_DEG).to_s + ',' + igcp.baro_alt.to_s + "\n"         # push data to array
-    #@ps<<[igcp.dlat.to_s,igcp.dlon.to_s]         # push data to array
-    #puts [igcp.dlat.to_s,igcp.dlon.to_s].inspect
-    # end
-
     # Cycle through directory
-    roots.each do |r]
+    roots.each do |r|
       Dir.foreach(r + "*") do |e|
         entry = path + "/" + e
 
@@ -94,8 +74,87 @@ class GearthController < ApplicationController
     end
   end
 
+  #http://localhost:3000/gearth/network_link3?path=public/data
 
-  def igc
+  def network_link3
+    puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    puts 'network_link3 ' + params.inspect
+
+    @entries = []
+
+    if params[:path].nil?
+      roots = []
+
+      @igcfs = Igcfile.find(:all, :order => 'path')  # get files
+      @igcfs.each do |igcf|    # for each file
+        #puts igcf.path
+        update_substr(roots ,igcf.path)
+      end
+      puts roots.inspect
+
+      roots.each do |r|
+        entry = r
+
+        if entry.last == "."
+          #puts 'ignoring ' + entry
+          next
+        end
+
+        puts entry.inspect
+        if File.directory?(entry)
+          #puts 'dir2 ' + entry
+          @entries << entry
+          next
+        end
+
+        if File.file?(entry)
+          if entry.downcase.match(".igc")
+            #puts 'igc ' + entry
+            @entries << entry
+            next
+          end
+        end
+      end
+    end
+
+    if not params[:path].nil?
+      # Cycle through directory
+      Dir.foreach(params[:path]) do |e|
+        entry = params[:path] + "/" + e
+
+        if entry.last == "."
+          #puts 'ignoring ' + entry
+          next
+        end
+
+        puts entry.inspect
+        if File.directory?(entry)
+          #puts 'dir2 ' + entry
+          @entries << entry
+          next
+
+        end
+
+        if File.file?(entry)
+          if entry.downcase.match(".igc")
+            #puts 'igc ' + entry
+            @entries << entry
+            next
+          end
+        else
+          puts "THIS FILE NO LONGER EXISTS" + entry
+        end
+      end
+    end
+
+    respond_to do |format|
+      #format.html # index.html.erb
+      format.kml  # index.kml.builder
+    end
+  end
+
+
+  def igc_off
     #puts 'network_link3' + Constants::XX
     path = "public/data"
     path = "c:/Users/peter/workspace_rails/igcsmall"
